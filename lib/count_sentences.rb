e1ce7fd5084cf52537bf0f8ee332259a4cb49e81
allison cortez <allizetorc@gmail.com>
2019-10-29 19:17:49 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -22,7 +22,7 @@ class String
 # string.scan(/[^\.!?]+[\.!?]/).map(&:strip).count # scan has regex to split string and strip will remove trailing spaces.
 # # => 4 
     
-    self.split(/[^\.!?]+[\.!?]/).count 
+    self.split(/[^\.!?]+[\.!?]/)
     
     
 # Remove empty elements from the array.
