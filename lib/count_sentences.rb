fbcec38bc810497b5cf5512539c18fbf574fb6e3
allison cortez <allizetorc@gmail.com>
2019-10-29 18:57:00 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    #1. split string (on any periods/questionmarks/exclamations)
+    #2. count the number of elements (that result from that string)
+    #remember: consider edge cases.. (this includes commas or extra punctuation)
+
+# string.scan(/[^\.!?]+[\.!?]/).map(&:strip).count # scan has regex to split string and strip will remove trailing spaces.
+# # => 4 
+
+    # new_array = count_sentences.split(/[^\.!?] + [\.!?]/)
+    
+    count_sentences.split(/(\.|\?|\!)/)
+    
+    
+    
+# Remove empty elements from the array.
+# elements.delete_if{|e| e.length == 0}
+# print elements
 
   end
 end
